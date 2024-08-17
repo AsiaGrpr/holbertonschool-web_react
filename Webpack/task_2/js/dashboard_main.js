47abaa12a3795a3198eebd810e72f066c7085484
AsiaGrpr <groupierre.asia@gmail.com>
2024-08-17 17:09:34 +0000

    Create a specific configuration for Webpack

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,20 +1,23 @@
-import '../css/main.css';
-import $ from 'jquery';
-import _ from 'lodash';
+const $ = require("jquery");
+const _ = require("lodash");
+import "../css/main.css";
 
-$('body').append('<div id="logo"></div>');
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$("body").append('<div id="logo"></div>');
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
+$("body").append("<p>Copyright - Holberton School</p>");
 
 let count = 0;
 function updateCounter() {
-  count++;
-  $('#count').text(`${count} clicks on the button`);
+	count++;
+	$("#count").text(`${count} clicks on the button`);
 }
-$('button').on('click', _.debounce(updateCounter, 500, {
-  'leading': true,
-  'trailing': false
-}));
+$("button").on(
+	"click",
+	_.debounce(updateCounter, 500, {
+		leading: true,
+		trailing: false,
+	})
+);
